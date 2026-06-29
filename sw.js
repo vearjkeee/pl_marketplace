54ca52ee078d5f1d8360e77e8d08719cfc95c912
vearjkeee <vearjkeee@gmail.com>
2026-06-29 22:46:40 +0300

    Change CACHE_NAME to 'pl-warehouse-v9'
    Updated cache name for PWA to version 9.

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Запросы к GAS всегда идут в сеть (должны быть свежие).
  */
 
-const CACHE_NAME = 'pl-warehouse-v1';
+const CACHE_NAME = 'pl-warehouse-v9';
 const ASSETS = [
   './',
   './index.html',
